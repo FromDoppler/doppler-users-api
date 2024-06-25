b79b3e58cacaed68f0f3dab200d8d4ce43a698b8
fgonzalez <fgonzalez@makingsense.com>
2024-06-25 16:10:03 -0300

    fix: rename file

--- Doppler.UsersApi/Model/InvitationInformation.cs
+++ Doppler.UsersApi/Model/InvitationInformation.cs
@@ -1,14 +1,15 @@
-using Newtonsoft.Json;
 using System;
 
 namespace Doppler.UsersApi.Model
 {
     public class InvitationInformation
     {
+        public int IdUser { get; set; }
         public string Email { get; set; }
         public string Firstname { get; set; }
         public string Lastname { get; set; }
         public DateTime InvitationDate { get; set; }
+        public DateTime ExpirationDate { get; set; }
         public int InvitationStatus { get; set; }
     }
 }
