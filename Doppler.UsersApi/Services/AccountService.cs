da8ab250d9af97b2f829916ec1b6ac25935c7184
fgonzalez <fgonzalez@makingsense.com>
2024-07-01 11:45:45 -0300

    chore: use new settings value

--- Doppler.UsersApi/Services/AccountService.cs
+++ Doppler.UsersApi/Services/AccountService.cs
@@ -1,24 +1,34 @@
 using Doppler.UsersApi.Infrastructure;
 using Doppler.UsersApi.Model;
 using Doppler.UsersApi.Model.Enums;
+using Microsoft.Extensions.Options;
 using System;
+using System.Reflection.Metadata.Ecma335;
 
 namespace Doppler.UsersApi.Services
 {
     public class AccountService : IAccountService
     {
         private readonly IAccountRepository _accountRepository;
+        private readonly AccountServiceSettings _settings;
 
-        public AccountService(IAccountRepository accountRepository)
+        public AccountService(IAccountRepository accountRepository, IOptions<AccountServiceSettings> settings)
         {
             _accountRepository = accountRepository;
+            _settings = settings.Value;
         }
 
         public void SendCollaborationInvite(CollaborationInvite invite, bool isNewInvite)
         {
             if (isNewInvite)
             {
-                _accountRepository.CreateCollaborationInvite(invite);
+                invite.ExpirationDate = DateTime.UtcNow.AddSeconds(_settings.InvitationExpirationSeconds);
+                var response = _accountRepository.CreateCollaborationInvite(invite);
+
+                if(response.Result == 0)
+                {
+                    throw new Exception("Failed to create invitation.");
+                }
             }
             else
             {
@@ -27,10 +37,15 @@ namespace Doppler.UsersApi.Services
                     throw new Exception("User has already an approved or pending invitation");
                 }
 
-                invite.ExpirationDate = DateTime.UtcNow.AddDays(1);
+                invite.ExpirationDate = DateTime.UtcNow.AddSeconds(_settings.InvitationExpirationSeconds);
                 invite.InviteStatus = (int)InviteStatusEnum.PENDING;
 
-                _accountRepository.UpdateCollaborationInvite(invite);
+                var response = _accountRepository.UpdateCollaborationInvite(invite);
+
+                if (response.Result == 0)
+                {
+                    throw new Exception("Failed to update invitation.");
+                }
             }
 
             //TODO: SendEmail(invite.Email);
